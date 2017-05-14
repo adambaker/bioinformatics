2a24fc9f0d8bcd1c0103c0382e53bf38600d147f
Adam C. Baker <adam.baker@workday.com>
2017-05-14 13:06:53 -0700

    solve second problem

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,19 @@
-module Lib
-    ( patternCount
+{-# LANGUAGE RankNTypes #-}
+module Lib(
+    patternCount,
+    freqWords
     ) where
 
-import Prelude hiding (tail, length)
-import Data.ByteString (ByteString, tail, length, isPrefixOf)
+import Prelude hiding (tail, length, take)
+import Data.ByteString (ByteString, tail, length, isPrefixOf, take)
+import Data.List (sortBy)
+import Data.Maybe (fromMaybe)
+import Data.HashTable.Class as H
+import Data.HashTable.ST.Basic as HT
+import Control.Monad.ST (ST, runST)
+
+type Counts s = HT.HashTable s ByteString Int
+type CountsST s = ST s (Counts s)
 
 patternCount :: ByteString -> ByteString -> Int
 patternCount text pattern = let
@@ -13,3 +23,27 @@ patternCount text pattern = let
     | length text' < patLen = count
     | otherwise = loop (count + fromEnum (isPrefixOf pattern text')) (tail text')
   in loop 0 text
+
+freqWords :: ByteString -> Int -> [ByteString]
+freqWords text k = mostFreq $ Lib.toList (H.new >>= countAll text k)
+
+countAll :: ByteString -> Int -> Counts s -> CountsST s
+countAll text k counts
+  | length text < k = return counts
+  | otherwise = inc counts (take k text) >>= countAll (tail text) k 
+
+inc :: Counts s -> ByteString -> CountsST s
+inc counts str = do
+  curr <- fmap (fromMaybe 0) (H.lookup counts str)
+  H.insert counts str (curr + 1)
+  return counts
+
+toList :: (forall s. CountsST s) -> [(ByteString, Int)]
+toList counts = runST $ counts >>= H.toList
+
+mostFreq :: [(ByteString, Int)] -> [ByteString]
+mostFreq [] = []
+mostFreq xs = let
+  sorted = sortBy (\(_, x) (_, y) -> y `compare` x) xs 
+  highest = snd (head sorted)
+  in map fst $ takeWhile (\x -> (snd x) == highest) sorted
