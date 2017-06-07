91f5fdd5eed3e31234cb6e388920a89087a1c3ac
Adam C. Baker <adam.durandal@gmail.com>
2017-06-07 10:34:26 -0700

    more of week 3

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,13 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as BS
-import Week3 (motifEnum)
+import Week3 (mostProbableStr, Profile, ProfileCol(..))
+import Control.Applicative (ZipList(..), (<*>), (<$>))
 
 main :: IO ()
 main = do
-  kd : dnas <- fmap BS.lines BS.getContents
-  let k : d : _ = map (read . BS.unpack) $ BS.words kd
-  BS.putStr $ BS.intercalate " " $  motifEnum k d dnas
+  dna : _ : profstrs <- fmap BS.lines BS.getContents
+  let parseDoubles = (map (read . BS.unpack) . BS.words :: BS.ByteString -> [Double])
+  let [as, cs, gs, ts] = (map (ZipList . parseDoubles) profstrs :: [ZipList Double])
+  let profile = ((getZipList $ ProfileCol <$> as <*> cs <*> gs <*> ts) :: Profile)
+  print $ mostProbableStr profile dna
--- src/Week3.hs
+++ src/Week3.hs
@@ -1,9 +1,11 @@
-module Week3(motifDist, motifEnum, median, kMers) where
+module Week3(motifDist, motifEnum, median, kMers, profileCol, ProfileCol(..),
+Profile, window, mostProbableStr) where
 
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import Week2 (hamming)
 import FreqArray(intToPattern)
+import Data.Word (Word)
 
 dist :: ByteString -> ByteString -> Int
 dist pat dna
@@ -30,3 +32,42 @@ median k dna = snd $ foldl step (maxBound, "") $ kMers k where
   step :: (Int, ByteString) -> ByteString -> (Int, ByteString)
   step (minDist, minPat) pat = let d = motifDist pat dna in
     if d < minDist then (d, pat) else (minDist, minPat)
+
+data ProfileCol = ProfileCol {a :: Double, c :: Double, g :: Double, t :: Double}
+type Profile = [ProfileCol]
+
+profileCol :: (Word, Word, Word, Word) -> ProfileCol
+profileCol (a', c', g', t') = let
+  toDouble :: Word -> Double
+  toDouble = toEnum . fromEnum
+  tot = toDouble $ a' + c' + g' + t'
+  [a, c, g, t] = map ((/tot) . toDouble) [a', c', g', t']
+  in ProfileCol a c g t
+
+profileColChars :: [Char] -> ProfileCol
+profileColChars = profileCol . foldl incChar (0, 0, 0, 0) where
+  incChar :: (Word, Word, Word, Word) -> Char -> (Word, Word, Word, Word)
+  incChar (a, c, g, t) 'A' = (succ a, c, g, t)
+  incChar (a, c, g, t) 'C' = (a, succ c, g, t)
+  incChar (a, c, g, t) 'G' = (a, c, succ g, t)
+  incChar (a, c, g, t) 'T' = (a, c, g, succ t)
+
+formProfile :: [ByteString] -> Profile
+formProfile strs = profileColChars (map BS.head strs) : formProfile (map BS.tail strs)
+
+pChar :: ProfileCol -> Char -> Double
+pChar prof 'A' = a prof
+pChar prof 'C' = c prof
+pChar prof 'G' = g prof
+pChar prof 'T' = t prof
+
+pStr :: Profile -> ByteString -> Double
+pStr profile str = product $ zipWith pChar profile (BS.unpack str)
+
+window :: Int -> ByteString -> [ByteString]
+window k str
+  | BS.length str == k = [str]
+  | otherwise = BS.take k str : window k (BS.tail str)
+
+mostProbableStr :: Profile -> ByteString -> ByteString
+mostProbableStr prof = snd . maximum . map (\s -> (pStr prof s, s)) . window (length prof)
